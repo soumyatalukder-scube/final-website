7d5165529ffcb503c4e6a9946958cf6daa4b0ff4
soumyatalukder-scube <soumya.talukder@gmail.com>
2026-06-24 16:14:54 +0530

    Add files via upload

---
+++ site-config.js
@@ -0,0 +1,11 @@
+/*
+ * Runtime website configuration.
+ *
+ * Update contactEmail before building or edit site-config.js directly in the
+ * deployed files. The first FormSubmit submission will send a confirmation
+ * message to this address. Automatic delivery starts after confirmation.
+ */
+window.SCUBE_CONFIG = {
+  contactEmail: "contact@scubeaiconsulting.com",
+  formEndpoint: "",
+};
